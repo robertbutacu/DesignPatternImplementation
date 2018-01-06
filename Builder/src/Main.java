9b3e515519ba5cd720a486359d6fdc9d982c5e13
robertbutacu <robert.butacu@yahoo.com>
2018-01-06 14:39:43 +0200

    builder()

--- Builder/src/Main.java
+++ Builder/src/Main.java
@@ -1,6 +1,28 @@
+import builder.Nationality;
+import builder.Person;
+import builder.PersonBuilder;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
+
+import java.text.ParseException;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person = new PersonBuilder()
+                .with(personBuilder -> {
+                    personBuilder.setFirstname("Mr.");
+                    personBuilder.setLastname("John");
+                    personBuilder.setNationality(Nationality.En);
+                    personBuilder.setProgrammer(true);
+                    try {
+                        personBuilder.setBirthdate("01-01-1900");
+                    } catch (InvalidValue | ParseException invalidValue) {
+                        invalidValue.printStackTrace();
+                    }
+                })
+                .createPerson();
+
+        System.out.println(person);
     }
 }
