1bba78d04391c64048411caf904bd9dfcfc58d3f
robertbutacu <robert.butacu@yahoo.com>
2018-01-09 21:06:05 +0200

    adapter design pattern

--- Adapter/src/Main.java
+++ Adapter/src/Main.java
@@ -1,6 +1,30 @@
+import Adapter.Adaptor;
+import Adapter.Civilian;
+import Adapter.Movement;
+import Adapter.Soldiers.General;
+import Adapter.Soldiers.Order;
+import Adapter.Soldiers.Soldier;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        General general = new General();
+
+        //just a simple peasant
+        Movement person = new Civilian();
+
+        person.walk();
+
+        System.out.println("-- Citizen has joined the army, his life is about to get a whole lot more interesting --");
+
+        person = new Adaptor(new Soldier(), general);
+
+        person.walk();
+        general.setOrder(Order.STROLLING);
+        person.walk();
+
+        general.setOrder(Order.RUNNING);
+
+        person.walk();
     }
 }
