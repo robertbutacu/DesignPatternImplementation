b20d3d9b364a76ebdd5ba993db501a0fe3ab213f
robertbutacu <robert.butacu@yahoo.com>
2018-01-13 22:48:31 +0200

    factory DP

--- Factory/src/Main.java
+++ Factory/src/Main.java
@@ -1,6 +1,8 @@
+import factory.Roulette;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        new Roulette().run();
     }
 }
