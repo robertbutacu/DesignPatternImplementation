c68ccb17e3cd59eff2d5bbe6c0180fe80dd07e82
robertbutacu <robert.butacu@yahoo.com>
2018-01-14 22:30:36 +0200

    Proxy added

--- Proxy/src/Main.java
+++ Proxy/src/Main.java
@@ -1,6 +1,38 @@
+import proxy.UltraHDVideo;
+import proxy.Video;
+import proxy.VideoProxy;
+
+import java.sql.Time;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Trying the proxy version first - 3 videos and playing just 1");
+        long startTime = System.currentTimeMillis();
+
+        Video v1 = new VideoProxy("Erik Mongrain - Alone in the mist");
+        Video v2 = new VideoProxy("Antoine Dufour - Drowning");
+        Video v3 = new VideoProxy("Jake McGuire - For Scale The Summit");
+
+        v3.play();
+        long endTime = System.currentTimeMillis();
+
+        long duration = (endTime - startTime);
+
+        System.out.println("It took " + (duration / 1000) + " seconds to run");
+        System.out.println("Now for the not-proxy version");
+        startTime = System.currentTimeMillis();
+
+        Video v4 = new UltraHDVideo("Erik Mongrain - Alone in the mist");
+        Video v5 = new UltraHDVideo("Antoine Dufour - Drowning");
+        Video v6 = new UltraHDVideo("Jake McGuire - For Scale The Summit");
+
+        v6.play();
+
+        endTime = System.currentTimeMillis();
+
+        duration = endTime - startTime;
+
+        System.out.println("It took " + (duration / 1000.0) + " seconds to run");
     }
 }
---
+++ Proxy/src/proxy/Video.java
@@ -0,0 +1,5 @@
+package proxy;
+
+public interface Video {
+    void play();
+}
